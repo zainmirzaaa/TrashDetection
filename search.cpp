16b2eb6ac989a3ef788156997c171558b658f950
zainmirzaaa <zainnmirzaa@gmail.com>
2025-01-11 14:42:00 -0600

    frontier search

---
+++ search.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+struct Cell {
+    int x, y;
+    bool visited;
+};
+
+int rows = 10;
+int cols = 10;
+vector<vector<Cell>> grid(rows, vector<Cell>(cols));
+
+vector<pair<int,int>> directions = {{0,1},{1,0},{0,-1},{-1,0}};
+
+bool isValid(int x, int y) {
+    return x >= 0 && x < rows && y >= 0 && y < cols && !grid[x][y].visited;
+}
+
+void frontierSearch(int startX, int startY) {
+    queue<pair<int,int>> q;
+    q.push({startX, startY});
+    grid[startX][startY].visited = true;
+
+    while(!q.empty()) {
+        auto [x, y] = q.front(); q.pop();
+        cout << "Drone visiting: (" << x << "," << y << ")\n";
+
+        for(auto [dx, dy] : directions) {
+            int nx = x + dx;
+            int ny = y + dy;
+            if(isValid(nx, ny)) {
+                grid[nx][ny].visited = true;
+                q.push({nx, ny});
+            }
+        }
+    }
+}
+
+int main() {
+    frontierSearch(0,0);
+    return 0;
+}
